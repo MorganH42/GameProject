b6fe9103247a62fbd19e123d1c556768c8553120
StolenKneecaps1 <Josh_s_quinn@yahoo.com>
2026-01-31 17:52:00 +0000

    PickUpPutDown
    Player can pick up and put down object, can only hold one at a time. Also added adjustable camera sensitivity

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -21,6 +21,15 @@ public class PlayerScript : MonoBehaviour
     RaycastHit interactData;
     [SerializeField]
     GameObject objectInteracted;
+    [SerializeField]
+    float verticalSensitivity = 1;
+    [SerializeField]
+    float horizontalSensitivity = 1;
+    [SerializeField]
+    GameObject itemHeld;
+    Rigidbody itemHeldRigidbody;
+    [SerializeField]
+    GameObject hands;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -30,12 +39,13 @@ public class PlayerScript : MonoBehaviour
         cameraStartPos = headCam.transform.position.y;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        hands = GameObject.Find("hands");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+        transform.Rotate(0, Input.GetAxis("Mouse X") * horizontalSensitivity, 0);
         moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         moveDir = transform.rotation * moveDir;
         if (Input.GetKey(KeyCode.LeftShift))
@@ -57,19 +67,44 @@ public class PlayerScript : MonoBehaviour
         }
         if (Input.GetMouseButtonDown(0))
         {
-            interactRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-            if (Physics.Raycast(interactRay, out interactData))
+            if (itemHeld == null)
             {
-                objectInteracted = interactData.transform.gameObject;
+                interactRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+                if (Physics.Raycast(interactRay, out interactData))
+                {
+                    objectInteracted = interactData.transform.gameObject;
+                    if (objectInteracted.tag == "CanHold")
+                    {
+                        itemHeld = objectInteracted;
+                        objectInteracted = null;
+                        itemHeldRigidbody = itemHeld.GetComponent<Rigidbody>();
+                        itemHeldRigidbody.useGravity = false;
+                        itemHeldRigidbody.isKinematic = true;
+                        itemHeld.transform.position = hands.transform.position;
+                        itemHeld.transform.parent = hands.transform;
+                    }
 
-            }
-            else
-            {
-                objectInteracted = null;
+                }
+                else
+                {
+                    objectInteracted = null;
+                }
             }
         }
-        xAxis -= Input.GetAxis("Mouse Y");
-        xAxis = Mathf.Clamp(xAxis, -80, 80);
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (itemHeld != null)
+            {
+                Debug.Log("handsfull");
+                itemHeldRigidbody.useGravity = true;
+                itemHeldRigidbody.isKinematic = false;
+                itemHeld.transform.parent = null;
+                itemHeld = null;
+                itemHeldRigidbody = null;
+            }
+        }
+        xAxis -= Input.GetAxis("Mouse Y") * verticalSensitivity;
+        xAxis = Mathf.Clamp(xAxis, -80, 60);
         headCam.transform.localRotation = Quaternion.Euler(xAxis, 0, 0);
         headCam.transform.localPosition = new Vector3(headCam.transform.localPosition.x, cameraStartPos + (Wave(bobMagnitude, bobSpeed) * Mathf.Abs(Input.GetAxis("Vertical"))), headCam.transform.localPosition.z);
     }
